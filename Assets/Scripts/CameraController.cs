5834841c4f7ea51303e000ade59bd0d9a9708506
Ramone Henry <ramoneh94@gmail.com>
2023-06-02 19:01:34 -0400

    add sprites for health, start adding health bar

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     // Room camera
     [SerializeField]
     private float speed;
@@ -14,9 +13,15 @@ public class CameraController : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
 
     // Follow Player
-    [SerializeField] private Transform player;
-    [SerializeField] private float aheadDistance;
-    [SerializeField] private float cameraSpeed;
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float aheadDistance;
+
+    [SerializeField]
+    private float cameraSpeed;
+
     private float lookAhead;
 
     // Start is called before the first frame update
@@ -28,10 +33,24 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         // alternate room by room camera movement
-        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        // transform.position =
+        //     Vector3
+        //         .SmoothDamp(transform.position,
+        //         new Vector3(currentPosX,
+        //             transform.position.y,
+        //             transform.position.z),
+        //         ref velocity,
+        //         speed);
+        transform.position =
+            new Vector3(player.position.x + lookAhead,
+                transform.position.y,
+                transform.position.z);
 
-        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        lookAhead =
+            Mathf
+                .Lerp(lookAhead,
+                (aheadDistance * player.localScale.x),
+                Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentHealth = startingHealth; 
+        
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        currentHealth -= _damage;
+
+        if(currentHealth > 0)
+        {
+            // player hurt, but alive
+        } else
+        {
+            // player has died 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
