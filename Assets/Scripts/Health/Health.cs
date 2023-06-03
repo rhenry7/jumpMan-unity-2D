5337c5771274cbdc519da499074907ce47f2a179
Ramone Henry <ramoneh94@gmail.com>
2023-06-02 21:53:50 -0400

    test take damage for player health

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField]
+    private float startingHealth;
 
-    [SerializeField] private float startingHealth;
-    private float currentHealth;
+    public float currentHealth { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
-
-        currentHealth = startingHealth; 
-        
+        currentHealth = startingHealth;
     }
 
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-        currentHealth -= _damage;
 
-        if(currentHealth > 0)
+        //currentHealth -= _damage;
+        if (currentHealth > 0)
         {
             // player hurt, but alive
-        } else
+        }
+        else
         {
-            // player has died 
+            // player has died
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
     }
 }
---
+++ Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Health playerHealth;
+
+    [SerializeField]
+    private Image totalHealthBar;
+
+    [SerializeField]
+    private Image currentHealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
