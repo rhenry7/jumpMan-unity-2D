ace1915509b8af2a83f4999e18783e7d7908fac6
Ramone Henry <ramoneh94@gmail.com>
2023-06-10 09:53:51 -0400

    update iframes logic

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -12,6 +12,8 @@ public class Health : MonoBehaviour
     [SerializeField]
     private float iFramesDuration;
 
+    private bool isInvincible = false;
+
     [SerializeField]
     private float numberOfFlashes;
 
@@ -51,6 +53,7 @@ public class Health : MonoBehaviour
 
     public void TakeDamage(float _damage)
     {
+        if (isInvincible) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         //currentHealth -= _damage;
@@ -58,7 +61,8 @@ public class Health : MonoBehaviour
         {
             // trigger when damage
             animation.SetTrigger("Hurt");
-            StartCoroutine(Invincibility());
+            StartCoroutine(BecomeTemporarilyInvincible());
+            //StartCoroutine(Invincibility());
             // iframes
         }
         else
@@ -72,11 +76,26 @@ public class Health : MonoBehaviour
         }
     }
 
-    private IEnumerator Invincibility()
+    // private IEnumerator Invincibility()
+    // {
+    //     Physics2D.IgnoreLayerCollision(10, 11, true);
+    //     // invincibility duration
+    //     for (int i = 0; i < numberOfFlashes; i++)
+    //     {
+    //         spriteRend.color = new Color(1, 0, 0, 0.5f);
+    //         yield return new WaitForSeconds(iFramesDuration /
+    //                 (numberOfFlashes * 2));
+    //         spriteRend.color = Color.white;
+    //         yield return new WaitForSeconds(iFramesDuration /
+    //                 (numberOfFlashes * 2));
+    //     }
+    //     Physics2D.IgnoreLayerCollision(10, 11, false);
+    // }
+    private IEnumerator BecomeTemporarilyInvincible()
     {
-        Physics2D.IgnoreLayerCollision(10, 11, true);
+        Debug.Log("Player turned invincible!");
+        isInvincible = true;
 
-        // invincibility duration
         for (int i = 0; i < numberOfFlashes; i++)
         {
             spriteRend.color = new Color(1, 0, 0, 0.5f);
@@ -86,7 +105,9 @@ public class Health : MonoBehaviour
             yield return new WaitForSeconds(iFramesDuration /
                     (numberOfFlashes * 2));
         }
-        Physics2D.IgnoreLayerCollision(10, 11, true);
+
+        isInvincible = false;
+        Debug.Log("Player is no longer invincible!");
     }
 
     // Update is called once per frame
