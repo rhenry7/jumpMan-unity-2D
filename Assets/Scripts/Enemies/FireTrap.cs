d87b68f5033a69411a82668e5b579aee5fde4761
Ramone Henry <ramoneh94@gmail.com>
2023-06-10 12:21:34 -0400

    update firetrap animation and IEnumerator function

--- Assets/Scripts/Enemies/FireTrap.cs
+++ Assets/Scripts/Enemies/FireTrap.cs
@@ -11,13 +11,16 @@ public class FireTrap : MonoBehaviour
     [SerializeField]
     private float activeTime;
 
+    [SerializeField]
+    private float damage;
+
     private Animator anim;
 
     private SpriteRenderer spriteRend;
 
     private bool triggered;
 
-    private bool activate;
+    private bool active;
 
     // Start is called before the first frame update
     void Start()
@@ -33,8 +36,26 @@ public class FireTrap : MonoBehaviour
             if (!triggered)
             {
                 // trigger
+                StartCoroutine(ActivateFireTrap());
+            }
+            if (active)
+            {
+                // trigger
+                collision.GetComponent<Health>().TakeDamage(damage);
             }
         }
+    }
+
+    private IEnumerator ActivateFireTrap()
+    {
+        triggered = true;
+        spriteRend.color = Color.red;
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white;
+        active = true;
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
     }
 
     // Update is called once per frame
