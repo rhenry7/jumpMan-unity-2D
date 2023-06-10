435db3ed2dcfc8bf158172b723a8dca7146c0c6d
Ramone Henry <ramoneh94@gmail.com>
2023-06-09 23:21:36 -0400

    improve temp invincibility when injured

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -80,6 +80,11 @@ public class Health : MonoBehaviour
         for (int i = 0; i < numberOfFlashes; i++)
         {
             spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration /
+                    (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration /
+                    (numberOfFlashes * 2));
         }
         Physics2D.IgnoreLayerCollision(10, 11, true);
     }
