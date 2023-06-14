8ae8aa7d54ae38e7f5aba31b7600bd302c92f984
Ramone Henry <ramoneh94@gmail.com>
2023-06-13 20:28:48 -0400

    add room script to clear enemies on room change
    '
    /'

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField]
+    private Transform previousRoom;
 
-    [SerializeField] private Transform previousRoom;
-    [SerializeField] private Transform nextRoom;
-    [SerializeField] private CameraController cam;
+    [SerializeField]
+    private Transform nextRoom;
 
+    [SerializeField]
+    private CameraController cam;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             if (collision.transform.position.x < transform.position.x)
-                cam.MoveToNewRoom(nextRoom);
+            {
+                cam.MoveToNewRoom (nextRoom);
+                nextRoom.GetComponent<Room1>().ActivateRoom(true);
+                previousRoom.GetComponent<Room1>().ActivateRoom(false);
+            }
             else
-                cam.MoveToNewRoom(previousRoom);
+            {
+                cam.MoveToNewRoom (previousRoom);
+                previousRoom.GetComponent<Room1>().ActivateRoom(true);
+                nextRoom.GetComponent<Room1>().ActivateRoom(false);
+            }
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
